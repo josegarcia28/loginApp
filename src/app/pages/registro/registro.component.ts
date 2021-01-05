3c8db01a5da2d36a58559a54bf0c7bef53e54ebf
Jose Garcia <josegarcia28@gmail.com>
2021-01-05 19:37:10 +0100

    Version final de proyecto

---
+++ src/app/pages/registro/registro.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from 'src/app/models/usuario.model';
+import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+  recordarme = false;
+  usuario: UsuarioModel;
+
+  constructor(private auth: AuthService,
+              private router: Router) { }
+
+  ngOnInit() {
+    this.usuario = new UsuarioModel();
+  }
+
+  onSubmit(form: NgForm) {
+    if (form.invalid) { return; }
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Espere por favor...'
+    });
+    Swal.showLoading();
+
+    this.auth.usuarioNuevo( this.usuario )
+    .subscribe( resp => {
+      console.log( resp );
+      Swal.close();
+      if (this.recordarme) {
+        localStorage.setItem('email', this.usuario.email);
+      }
+      this.router.navigateByUrl('/home');
+    }, (err) => {
+      console.log(err.error.error.message);
+      Swal.fire({
+        icon: 'error',
+        title: 'Error al Autenticar',
+        text: err.error.error.message
+      });
+    });
+  }
+
+}
